d1697ad876f533eace1b171b9f716bca8843fccd
Weipeng Zhang <wp.zhang@hotmail.com>
2022-11-17 16:56:07 -0500

    :fire: Remove files

--- src/SLR/models/lightning_model.py
+++ src/SLR/models/lightning_model.py
@@ -1,143 +1,11 @@
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision.models as models
-from .modules import Decoder, SeqKD, BiLSTMLayer, TemporalConv
-from .evaluation import evaluate
-from pytorch_lightning import LightningModule
 import os
+import torch
 import torch.distributed as dist
+from pytorch_lightning import LightningModule
 
-
-class Identity(nn.Module):
-    def __init__(self):
-        super(Identity, self).__init__()
-
-    def forward(self, x):
-        return x
-
-
-class NormLinear(nn.Module):
-    def __init__(self, in_dim, out_dim):
-        super(NormLinear, self).__init__()
-        self.weight = nn.Parameter(torch.Tensor(in_dim, out_dim))
-        nn.init.xavier_uniform_(self.weight, gain=nn.init.calculate_gain("relu"))
-
-    def forward(self, x):
-        outputs = torch.matmul(x, F.normalize(self.weight, dim=0))
-        return outputs
-
-
-class SLRModel(nn.Module):
-    def __init__(
-        self,
-        num_classes,
-        c2d_type,
-        conv_type,
-        use_bn=False,
-        hidden_size=1024,
-        gloss_dict=None,
-        weight_norm=True,
-        share_classifier=True,
-    ):
-        super(SLRModel, self).__init__()
-
-        self.num_classes = num_classes
-        self.conv2d = getattr(models, c2d_type)(pretrained=True)
-        self.conv2d.fc = Identity()
-        self.conv1d = TemporalConv(
-            input_size=512,
-            hidden_size=hidden_size,
-            conv_type=conv_type,
-            use_bn=use_bn,
-            num_classes=num_classes,
-        )
-
-        self.decoder = Decoder(gloss_dict, num_classes, "beam")
-        self.temporal_model = BiLSTMLayer(
-            rnn_type="LSTM",
-            input_size=hidden_size,
-            hidden_size=hidden_size,
-            num_layers=2,
-            bidirectional=True,
-        )
-        if weight_norm:
-            self.classifier = NormLinear(hidden_size, self.num_classes)
-            self.conv1d.fc = NormLinear(hidden_size, self.num_classes)
-        else:
-            self.classifier = nn.Linear(hidden_size, self.num_classes)
-            self.conv1d.fc = nn.Linear(hidden_size, self.num_classes)
-        if share_classifier:
-            self.conv1d.fc = self.classifier
-        self.register_full_backward_hook(self.backward_hook)
-
-    def backward_hook(self, module, grad_input, grad_output):
-        for g in grad_input:
-            g[g != g] = 0
-
-    def masked_bn(self, inputs, len_x):
-        def pad(tensor, length):
-            return torch.cat(
-                [
-                    tensor,
-                    tensor.new(length - tensor.size(0), *tensor.size()[1:]).zero_(),
-                ]
-            )
-
-        x = torch.cat(
-            [
-                inputs[len_x[0] * idx : len_x[0] * idx + lgt]
-                for idx, lgt in enumerate(len_x)
-            ]
-        )
-        x = self.conv2d(x)
-        x = torch.cat(
-            [
-                pad(x[sum(len_x[:idx]) : sum(len_x[: idx + 1])], len_x[0])
-                for idx, lgt in enumerate(len_x)
-            ]
-        )
-        return x
-
-    def forward(self, x, len_x, label=None, label_lgt=None):
-        if len(x.shape) == 5:
-            # videos
-            batch, temp, channel, height, width = x.shape
-            inputs = x.reshape(batch * temp, channel, height, width)
-            framewise = self.masked_bn(inputs, len_x)
-            framewise = framewise.reshape(batch, temp, -1).transpose(1, 2)
-        else:
-            # frame-wise features
-            framewise = x
-
-        conv1d_outputs = self.conv1d(framewise, len_x)
-        # x: T, B, C
-        x = conv1d_outputs["visual_feat"]
-        lgt = conv1d_outputs["feat_len"]
-        tm_outputs = self.temporal_model(x, lgt)
-        outputs = self.classifier(tm_outputs["predictions"])
-        pred = (
-            None
-            if self.training
-            else self.decoder.decode(outputs, lgt, batch_first=False, probs=False)
-        )
-        conv_pred = (
-            None
-            if self.training
-            else self.decoder.decode(
-                conv1d_outputs["conv_logits"], lgt, batch_first=False, probs=False
-            )
-        )
-
-        return {
-            # "framewise_features": framewise,
-            # "visual_features": x,
-            "feat_len": lgt,
-            "conv_logits": conv1d_outputs["conv_logits"],
-            "sequence_logits": outputs,
-            "conv_sents": conv_pred,
-            "recognized_sents": pred,
-        }
+from .modules import SeqKD
+from .evaluation import evaluate
+from .SLR_model import SLRModel
 
 
 class SLR_Lightning(LightningModule):
@@ -167,6 +35,8 @@ class SLR_Lightning(LightningModule):
         eval_label_dir,
     ):
         super().__init__()
+
+        self.save_hyperparameters()
 
         # * Define model
         self.model = SLRModel(
@@ -236,6 +106,12 @@ class SLR_Lightning(LightningModule):
         ret_dict = self.model(vid, vid_lgt, label, label_lgt)
         loss = self.calc_loss(ret_dict, label, label_lgt)
         self.log("train_loss", loss, batch_size=vid.size(0), on_step=True)
+        self.log(
+            "lr",
+            self.trainer.lr_schedulers.get_last_lr(),
+            batch_size=vid.size(0),
+            on_step=True,
+        )
         return loss
 
     def eval_step(self, batch, stage):
@@ -271,12 +147,6 @@ class SLR_Lightning(LightningModule):
             full_out = [device_out]
 
         if self.global_rank == 0:
-
-            # if self.trainer.num_devices > 1:
-            #     print(len(outputs))
-            #     outputs = self.all_gather(outputs)
-            #     print(len(outputs))
-
             total_info = []
             total_sent = []
             total_conv_sent = []
@@ -343,7 +213,7 @@ class SLR_Lightning(LightningModule):
                     # sync_dist=True,
                     on_epoch=True,
                 )
-                return {"WER": conv_ret}
+                return {f"{stage}_WER": conv_ret}
             except:
                 pass
                 # self.log("Unexpected error:", sys.exc_info()[0], sync_dist=True)
